29accfd597403665883205e97b13ac1d7e40fa79
Crystal <zhencq@126.com>
2021-04-06 09:50:18 +0800

    '增加html'

--- showdata.py
+++ showdata.py
@@ -10,28 +10,57 @@ def getlist(search,result):
             },
             ]
     
+    # path = "htmls"
+    # directory=path+"/"+search
+    # files = os.listdir(directory)
+    # for filename in files:
+    #     if os.path.isfile(directory+"/"+filename):
+    #         f = open(filename, 'r', encoding="utf-8")
+    #         lines = f.readlines()
+    #         length = len(lines)
+    #         i = 0
+    #         description = "abstract"
+    #         keywords="key1 key2"
+    #         origin = "localhost:8000"+"/"+"static"+"/"+search+"/"+filename
+    #         while i < length:
+    #             dictt = {
+    #                 "title": lines[i][:-1],
+    #                 "description": description,
+    #                 "keywords":keywords,
+    #                 "origin": origin
+    #             }
+    #     dataNews.append(dictt)
+        # print("kl",l)
+        # i += 2
 
-    path = "MilitaryShow/static"
+
+    path = "htmls"
 
     directory = path+"/"+str(search) #打开关键字对应目录
     files = os.listdir(directory)#列出该目录下所有文件名
     for filename in files:
-        print(filename)
-        length=len(filename)
-        title=filename[:length-6]
-        description="abstract"
-        keywords="key1 key2"
-        origin="localhost:8000"+"/"+"static"+"/"+search+"/"+filename
-        dictt={
-            "title":title,
-            "description":description,
-            "keywords":keywords,
-            "origin":origin
-        }
-        dataNews.append(dictt)
-    # print(directory)
+        if os.path.isfile(directory+"/"+filename):
+            print(filename)
+            length=len(filename)
+            # title=filename[:length-6]
+            title = filename
+            description="abstract"
+            keywords="key1 key2"
+            # origin="localhost:8000"+"/"+"static"+"/"+search+"/"+filename
+            origin = "http://127.0.0.1:8000/static/"+ filename
+            dictt={
+                "title":title,
+                "description":description,
+                "keywords":keywords,
+                "origin":origin
+            }
+            dataNews.append(dictt)
+    print("directory"+directory)
     
     # print(dataNews[1]["title"])
+    print(dataNews)
+
+
     return dataNews
 
 # search="神舟五号"
